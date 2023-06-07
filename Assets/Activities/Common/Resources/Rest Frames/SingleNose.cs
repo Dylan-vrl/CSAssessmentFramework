7a2f204f7b569b97aa81fc5720f33c0e5200946f
elifKurtay <elif.kurtay00@gmail.com>
2023-06-07 22:56:56 +0200

    clean scenes

--- Assets/Activities/Common/Resources/Rest Frames/SingleNose.cs
+++ Assets/Activities/Common/Resources/Rest Frames/SingleNose.cs
@@ -11,26 +11,19 @@ using UnityEditor;
 
 public class SingleNose : MonoBehaviour
 {
-    [SerializeField] private float yPosition;
-    [SerializeField] private float zPosition;
     [SerializeField] private float noseWidth;
     [SerializeField] private float noseFlatness;
 
-    [SerializeField] public float YPosition { get { return yPosition; } set { yPosition = value; } }
-    [SerializeField] public float ZPosition { get { return zPosition; } set { zPosition = value; } }
     [SerializeField] public float NoseWidth { get { return noseWidth; } set { noseWidth = value; } }
     [SerializeField] public float NoseFlatness { get { return noseFlatness; } set { noseFlatness = value; } }
 
     void Start()
     {
-        float zPos = Mathf.Lerp(0.4f, 0.8f, zPosition);
-        float yPos = Mathf.Lerp(-0.5f, 0.5f, yPosition);
         float xScale = Mathf.Lerp(0.05f, .15f, noseWidth);
         float yScale = Mathf.Lerp(0.05f, .25f, 1 - noseFlatness);
         float zScale = Mathf.Lerp(.03f, .15f, .5f);
 
         transform.localScale = new Vector3(xScale, yScale, zScale);
-        transform.localPosition = new Vector3(transform.localPosition.x, yPos, zPos);
     }
 
 }
