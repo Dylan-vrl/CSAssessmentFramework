ae142e30b47db621f91de8f36ede1f96ed7031c9
Dylan <dylan.vairoli@gmail.com>
2023-06-08 18:22:46 +0200

    Add presettable for Pixelize

---
+++ Assets/Pixelize.cs
@@ -0,0 +1,37 @@
+using System;
+using CSFramework.Core;
+using UnityEngine;
+using CSFramework.Presets;
+using UnityEngine.Rendering;
+using static CSFramework.Core.PresettableCategory;
+
+namespace CSFramework.Extensions
+{
+	[RequireComponent(typeof(Volume))]
+	public class Pixelize : Extension<Volume, PixelizePreset>
+	{
+		public override PresettableCategory GetCategory() => Vision;
+
+		private Volume _volume;
+
+		private void Awake()
+		{
+			_volume = GetComponent<Volume>();
+		}
+
+		private void OnEnable()
+		{
+			// If no PixelizeComponent found, create a new one
+			if (!_volume.profile. TryGet<PixelizeComponent>(out var pixelizeComponent))
+				pixelizeComponent = _volume.profile.Add<PixelizeComponent>();
+			
+			// Set the PixelizeComponent active
+			pixelizeComponent.active = true;
+			foreach (var param in pixelizeComponent.parameters)
+				param. overrideState = true;
+			
+			// Set the height to the preset one
+			pixelizeComponent.screenHeight.value = Preset. ScreenHeight;
+		}
+	}
+}
---
+++ Assets/PixelizePreset.cs
@@ -0,0 +1,13 @@
+using CSFramework.Core;
+using CSFramework.Extensions;
+using UnityEngine;
+
+namespace CSFramework.Presets
+{
+    [CreateAssetMenu(menuName = "CSFramework/Preset Instances/Vision/PixelizePreset", fileName = "new PixelizePreset")]
+    public class PixelizePreset: Preset<Pixelize>
+    {
+        [field: SerializeField]
+        public int ScreenHeight { get; private set; }
+    }
+}
